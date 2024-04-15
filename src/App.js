4739fcb6c98c1ed2c61c235acada126814bceda1
shakya94 <57297686+shakya94@users.noreply.github.com>
2024-04-15 22:18:50 +0530

    yourganik

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import coming from './Images/coming.png';
+
+
+function App() {
+  return (
+    <div className="App">
+      <img src={coming} height="800" width="1000"/>
+    </div>
+  );
+}
+
+export default App;
