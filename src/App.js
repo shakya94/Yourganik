2be2aa6a53cdc634f59cf50850f24766a4994d29
shakya94 <57297686+shakya94@users.noreply.github.com>
2024-04-16 18:22:11 +0530

    img fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 
 import './App.css';
-import website from './Images/website.png';
-
+import websiteb from './Images/websiteb.png';
+import mobile from './Images/mobile.png';
 
 
 function App() {
   return (
     <div className="App">
-      <img src={website} alt="your"/>
+       {/* For mobile view */}
+       <img src={mobile} alt="Mobile Image" className="mobile-image" />
+            
+            {/* For desktop view */}
+            <img src={websiteb} alt="Desktop Image" className="desktop-image" />
     </div>
   );
 }
